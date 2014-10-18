931dfdb0b578959237cd8f2392d09b7c1670b215
IgorSidorov <igoryakha@list.ru>
2014-10-18 19:55:05 +0400

    Added new libs

--- app/src/main/java/com/restomania/restomania/StartActivity.java
+++ app/src/main/java/com/restomania/restomania/StartActivity.java
@@ -36,6 +36,7 @@ public class StartActivity extends Activity {
     }
 
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
